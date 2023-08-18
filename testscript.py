351283216506c13d11ca0950cc1efc7dbc189d8d
jacksontabish <137221920+jacksontabish@users.noreply.github.com>
2023-08-18 11:36:46 -0700

    Update testscript.py

--- testscript.py
+++ testscript.py
@@ -1,4 +1,4 @@
-from PF3400 import PF3400Controller
+from PreciseArmLibrary import PF3400Controller
 import time
 
 def main():
@@ -6,49 +6,57 @@ def main():
    
     ip = "192.168.0.1"
     port = 10100
-    arm = PF3400Controller(ip,port)
+    arm = PF3400Controller(ip,port) 
   
-    arm.connect()
-    print("Connecting to TCP Command Server...")
-    arm.poweron()
-    print("Arm power on.")
+    arm.connect() #socket connection via Telnet
+    arm.powerOn()
     arm.home()
-    print("Robot homed.")
-    arm.attachrobot()
-    print("Robot attached.")
-    arm.WaitforEOM()
+    arm.attachRobot()
+    arm.waitForSync
+    
 
+    #set teachpoints and motion profile (can make multiple profiles)
+    arm.TeachPointProtocol()
     arm.setProfile(1,50,0,100,100,0.1,0.1,10,0)
-    print("Motion Profile 1 Defined.")
-    arm.setCartesianLoc(1,207,-139,-4,-134)
-    print("Teachpoint 1 set.")
-    arm.setCartesianLoc(2,238,-100,23,-119)
-    print("Teachpoint 2 set.")
-    arm.setCartesianLoc(3,100,-18,45,-100)
-    print("Teachpoint 3 set.")
 
-    print("Moving arm to Teachpoint 1.")
-    arm.moveAppro(1,1)
-    arm.move(1,1)
-    time.sleep(5)
-    print("Waiting 5 seconds.")
+    duration = 28800  #we want to execute a block of action for 8 hours
+    start_time = time.time()
+    while time.time() - start_time < duration:
+        #open gripper
+        arm.openGripper()
+        #move to teachpoint 1 with motion profile 1
+        arm.approAndMove(1,1)
+        #close gripper, pick the object up
+        arm.closeGripper()
+        #move to safe height (approach)
+        arm.Approach(1,1)
+        
+        #move to teachpoint 2
+        arm.approandMove(2,1)
+        #put the object down
+        arm.openGripper()
+        #move to safe height
+        arm.Approach(2,1)
+        
+        #move to teachpoint 3
+        arm.approAndMove(3,1)
+        #pick up an object
+        arm.closeGripper()
+        #move to safe height
+        arm.Approach(3,1)
+        #bring the object to teachpoint 1
+        arm.approandMove(1,1)
+        #put the object down 
+        arm.openGripper()
 
-    print("Moving arm to Teachpoint 2.")
-    arm.moveAppro(2,1)
-    arm.move(2,1)
-    time.sleep(5)
-    print("Waiting 5 seconds.")
-
-    print("Moving arm to Teachpoint 3.")
-    arm.moveAppro(3,1)
-    arm.move(3,1)
-    time.sleep(5)
-    print("Waiting 5 seconds.")
-
-
-
+      
+        pass
+   
+    #detach, power off, exit
     arm.detachrobot()
     arm.poweroff()
+    arm.exitTCP()
+    
 
 
     if __name__ == "__main__":
